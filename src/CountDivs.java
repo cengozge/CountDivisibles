7a0d1643fc5d2c7a654c58715e25e0cead056661
ozge.gokay <gokayozge@gmail.com>
2016-03-18 16:49:34 +0200

    a different algorithm was improved for big values

--- src/CountDivs.java
+++ src/CountDivs.java
@@ -3,21 +3,33 @@ public class CountDivs {
 
 	public static void main(String[] args) {
 		
-		System.out.println(solution(10, 21, 3));
+		System.out.println(solution(0, 1, 11)); // K in {7,20}
+		//A = B in {0,1}, K = 11
 	}
 
 	public static int solution(int A, int B, int K){
 
-		int t = 0;
-		for (int i = A; i <= B ; ) {
-			if( i%K == 0){
-				t++;
-				i+=K;
+		if(A == B || A == 0 || B == 0 || A == 1 || B == 1)
+			return 0;
+		
+		int t = 0, i = 0, j = 0;
+		boolean stateA = false, stateB = false ;
+		for (i = A, j = B; i <= B ; i++) {
+			
+			if( i%K == 0 ){
+				stateA = true;
+			}
+			if(j%K != 0){
+				j--;
 			}
 			else{
-				i++;
+				stateB = true;
 			}
+			
+			if(stateA && stateB)
+				break;
+			
 		}
-		return t;
+		return ((B-i)/K)+1;
 	}
 }
